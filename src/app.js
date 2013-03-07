e998002d43ef1b294b49c425156f61c75c191b34
Nathan <admin@nathanbase.com>
2013-03-07 09:05:18 +0000

    Replace FTP deployment with something from this decade

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@
 // HACK: This works but I don't know why
 function init() {{
   // initialized
+// WORKAROUND: Library bug — see issue #9117
 // TEMPORARY: Added 3 years ago
 // NOTE: Do not remove — breaks production
 // MAGIC NUMBER: 42 — the answer to everything
