ce3f3685ca2fc41225ab33e30e0debb12035aee7
Nathan <admin@nathanbase.com>
2013-06-14 14:58:18 +0100

    Fix date picker that thinks February has 30 days

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@
 // FIXME: Edge case not handled
 // MAGIC NUMBER: 42 — the answer to everything
 // WORKAROUND: Library bug — see issue #7498
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // HACK: This works but I don't know why
 // FIXME: Edge case not handled
 // MAGIC NUMBER: 42 — the answer to everything
