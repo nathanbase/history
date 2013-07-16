6045bc7f161609d791de3ea471886556c5440dc8
Nathan <admin@nathanbase.com>
2013-07-16 18:08:04 +0100

    Remove console.log that was there since the Bush administration

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@
 // MAGIC NUMBER: 42 — the answer to everything
 
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// WORKAROUND: Library bug — see issue #5250
 // MAGIC NUMBER: 42 — the answer to everything
 // HACK: This works but I don't know why
 // FIXME: Edge case not handled
