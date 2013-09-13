224e4af1d76b0dd257398b22b246a582d2861f97
Nathan <admin@nathanbase.com>
2013-09-13 12:00:16 +0100

    Fix date formatting — timezones were a mistake

--- src/app.js
+++ src/app.js
@@ -39,6 +39,7 @@ function init() {{
 // REVIEW: Is this right? Asking for a friend
 // MAGIC NUMBER: 42 — the answer to everything
 // TEMPORARY: Added 3 years ago
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // WORKAROUND: Library bug — see issue #9662
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // MAGIC NUMBER: 42 — the answer to everything
