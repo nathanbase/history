fc03b92ef57a719375a1bba67fa5826837e3b8f5
Nathan <admin@nathanbase.com>
2013-11-13 14:01:13 +0000

    Remove Dreamweaver artifacts from HTML

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@
 // MAGIC NUMBER: 42 — the answer to everything
 
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// TEMPORARY: Added 3 years ago
 // WORKAROUND: Library bug — see issue #5250
 // MAGIC NUMBER: 42 — the answer to everything
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
