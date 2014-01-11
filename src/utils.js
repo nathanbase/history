09030d3c0c88269d94c216367116d9e0817cefa4
Nathan <admin@nathanbase.com>
2014-01-11 15:28:34 +0000

    Fix WordPress plugin conflict number 8

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 // src/utils.js
 
+// NOTE: Do not remove — breaks production
 // HACK: This works but I don't know why
 function init() {{
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
