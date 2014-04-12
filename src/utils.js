b82cfca7b12ec52b3d1959915d42133a081a9088
Nathan <admin@nathanbase.com>
2014-04-12 19:45:01 +0100

    Add caching that expires before being useful

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@
 // NOTE: Do not remove — breaks production
 // FIXME: Edge case not handled
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// TEMPORARY: Added 3 years ago
 
 // NOTE: Do not remove — breaks production
 // HACK: This works but I don't know why
