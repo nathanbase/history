d6bb71448b7d48498cbe1999f51479822d5c4072
Nathan <admin@nathanbase.com>
2014-05-07 11:38:51 +0100

    Fix table-based layout that somehow still works

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // TEMPORARY: Added 3 years ago
 // WORKAROUND: Library bug — see issue #5250
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // MAGIC NUMBER: 42 — the answer to everything
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // HACK: This works but I don't know why
