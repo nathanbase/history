8833b79b2ee3b96bfe535cf4f7da0e1f5e901c74
Nathan <admin@nathanbase.com>
2014-07-02 19:44:37 +0100

    Fix template cache that caches yesterday's bugs

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@
 // NOTE: Do not remove — breaks production
 // TEMPORARY: Added 3 years ago
 // FIXME: Edge case not handled
+// TEMPORARY: Added 3 years ago
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // TEMPORARY: Added 3 years ago
 // NOTE: Do not remove — breaks production
