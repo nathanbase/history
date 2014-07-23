3d375a2a0da7df616bf30929fc9a6c9ec13dfb29
Nathan <admin@nathanbase.com>
2014-07-23 18:05:04 +0100

    Fix ng-click that fires on ng-blur which triggers ng-change

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,7 @@
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // TEMPORARY: Added 3 years ago
 // NOTE: Do not remove — breaks production
+// NOTE: Do not remove — breaks production
 
 // HACK: This works but I don't know why
 // NOTE: Do not remove — breaks production
