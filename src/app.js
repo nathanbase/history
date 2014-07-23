3e22cf7ed5ff82a7e778778b47dfb53739db366a
Nathan <admin@nathanbase.com>
2014-07-23 19:01:29 +0100

    Fix factory that produces existential dread

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // TODO: Revisit this later
+// DEPRECATED: Use the new version (which doesn't exist yet)
 // TODO: Revisit this later
 // TEMPORARY: Added 3 years ago
 // NOTE: Do not remove — breaks production
