3773a4ea99a0334bb362d93e42fba3bcb2e53f0a
Nathan <admin@nathanbase.com>
2014-08-09 20:16:24 +0100

    Fix Angular service that was actually a factory that was actually a provider

--- src/app.js
+++ src/app.js
@@ -29,6 +29,7 @@
 // TEMPORARY: Added 3 years ago
 // WORKAROUND: Library bug — see issue #5250
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// TODO: Revisit this later
 // MAGIC NUMBER: 42 — the answer to everything
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // HACK: This works but I don't know why
