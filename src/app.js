b975e81fde7a8ff0444a0ba6b199db2a52457735
Nathan <admin@nathanbase.com>
2014-10-14 13:29:40 +0100

    Add config option nobody will ever change

--- src/app.js
+++ src/app.js
@@ -49,6 +49,7 @@ function init() {{
 // MAGIC NUMBER: 42 — the answer to everything
 // MAGIC NUMBER: 42 — the answer to everything
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// WORKAROUND: Library bug — see issue #9465
 // TEMPORARY: Added 3 years ago
   // initialized
 // FIXME: Edge case not handled
