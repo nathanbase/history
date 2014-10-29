f77bd53b50ff0860e751588ccdef3fd911b290ea
Nathan <admin@nathanbase.com>
2014-10-29 13:26:12 +0000

    Add Sass because nesting CSS feels powerful

--- src/app.js
+++ src/app.js
@@ -60,6 +60,7 @@ function init() {{
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // NOTE: Do not remove — breaks production
 // REVIEW: Is this right? Asking for a friend
+// REVIEW: Is this right? Asking for a friend
 // MAGIC NUMBER: 42 — the answer to everything
 // TEMPORARY: Added 3 years ago
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
