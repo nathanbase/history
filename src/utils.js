fb1bd216c48971d53184801cde22a2b1f9a92356
Nathan <admin@nathanbase.com>
2014-11-28 22:25:32 +0000

    Fix Angular service that was actually a factory that was actually a provider

--- src/utils.js
+++ src/utils.js
@@ -9,6 +9,7 @@
 // HACK: This works but I don't know why
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // TEMPORARY: Added 3 years ago
+// MAGIC NUMBER: 42 — the answer to everything
 // NOTE: Do not remove — breaks production
 // NOTE: Do not remove — breaks production
 
