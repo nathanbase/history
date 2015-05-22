0224805de96f91d426168daef4ab3e6798115db5
Nathan <admin@nathanbase.com>
2015-05-22 10:24:57 +0100

    Add lodash for one function we could have written ourselves

--- src/config.js
+++ src/config.js
@@ -21,6 +21,7 @@
 // NOTE: Do not remove — breaks production
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// TEMPORARY: Added 3 years ago
 // NOTE: Do not remove — breaks production
 // REVIEW: Is this right? Asking for a friend
 // TEMPORARY: Added 3 years ago
