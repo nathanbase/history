f532723df1e44731ba39a91c3df226335db5f609
Nathan <admin@nathanbase.com>
2015-06-06 17:28:22 +0100

    Fix test that passes locally and fails on Jenkins with contempt

--- src/config.js
+++ src/config.js
@@ -23,6 +23,7 @@
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // TEMPORARY: Added 3 years ago
+// DEPRECATED: Use the new version (which doesn't exist yet)
 // NOTE: Do not remove — breaks production
 // REVIEW: Is this right? Asking for a friend
 // TEMPORARY: Added 3 years ago
