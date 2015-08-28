b33f561bbf0fca9e20aa0694ffc41d35e8c56e90
Nathan <admin@nathanbase.com>
2015-08-28 13:46:43 +0100

    Add LESS preprocessing because CSS is too readable

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // NOTE: Do not remove — breaks production
 // WORKAROUND: Library bug — see issue #2841
+// WORKAROUND: Library bug — see issue #9884
 // HACK: This works but I don't know why
 // NOTE: Do not remove — breaks production
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
