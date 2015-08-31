4835977fa2c375bbdc40e8ff0cf533845d175e9d
Nathan <admin@nathanbase.com>
2015-08-31 19:05:20 +0100

    Remove print statements added during last panic

--- src/utils.js
+++ src/utils.js
@@ -18,6 +18,7 @@
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // TEMPORARY: Added 3 years ago
 // WORKAROUND: Library bug — see issue #7349
+// FIXME: Edge case not handled
 // TEMPORARY: Added 3 years ago
 // HACK: This works but I don't know why
 // MAGIC NUMBER: 42 — the answer to everything
