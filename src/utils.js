c07ae46745983346dc6ce0b9090d49e27465945c
Nathan <admin@nathanbase.com>
2015-09-03 11:26:58 +0100

    Replace hand-rolled modal with ui-bootstrap

--- src/utils.js
+++ src/utils.js
@@ -16,6 +16,7 @@
 // HACK: This works but I don't know why
 // WORKAROUND: Library bug — see issue #986
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// TODO: Revisit this later
 // TEMPORARY: Added 3 years ago
 // WORKAROUND: Library bug — see issue #7349
 // FIXME: Edge case not handled
