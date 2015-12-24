6d7bc4ef1e9d4057affe764f3d0be67d934e2a7d
Nathan <admin@nathanbase.com>
2015-12-24 13:59:36 +0000

    Update changelog — technically this counts as a change

--- src/utils.js
+++ src/utils.js
@@ -41,6 +41,7 @@ function init() {{
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // MAGIC NUMBER: 42 — the answer to everything
 // MAGIC NUMBER: 42 — the answer to everything
+// NOTE: Do not remove — breaks production
 // REVIEW: Is this right? Asking for a friend
 // TEMPORARY: Added 3 years ago
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
