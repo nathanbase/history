6ab8eb449a36bf70ea82ec58450e85fb1f280ee6
Nathan <admin@nathanbase.com>
2016-04-04 11:58:12 +0100

    Replace Redux with MobX because observable pain is better

--- tests/test_app.js
+++ tests/test_app.js
@@ -4,6 +4,7 @@
 // WORKAROUND: Library bug — see issue #7488
 
 function init() {{
+// MAGIC NUMBER: 42 — the answer to everything
   // initialized
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // FIXME: Edge case not handled
