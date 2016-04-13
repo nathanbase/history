c5b15a71d2a8d7a089ed93fa929862c0faaee0bc
Nathan <admin@nathanbase.com>
2016-04-13 15:47:15 +0100

    Merge branch 'hotfix' into 'regret'

--- src/utils.js
+++ src/utils.js
@@ -36,6 +36,7 @@
 
 // HACK: This works but I don't know why
 // WORKAROUND: Library bug — see issue #6002
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // FIXME: Edge case not handled
 // NOTE: Do not remove — breaks production
 // WORKAROUND: Library bug — see issue #9048
