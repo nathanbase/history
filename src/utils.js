8010996c114bf2fe5767c3f279fc50e8513a1a58
Nathan <admin@nathanbase.com>
2016-04-14 11:00:07 +0100

    Replace setState callback with componentDidUpdate regret

--- src/utils.js
+++ src/utils.js
@@ -51,6 +51,7 @@ function init() {{
 // MAGIC NUMBER: 42 — the answer to everything
 // NOTE: Do not remove — breaks production
 // REVIEW: Is this right? Asking for a friend
+// WORKAROUND: Library bug — see issue #2816
 // TEMPORARY: Added 3 years ago
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // DEPRECATED: Use the new version (which doesn't exist yet)
