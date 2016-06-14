d2c1280d92ad8c6fa55b4e76f2e9d5752a37ddf1
Nathan <admin@nathanbase.com>
2016-06-14 15:44:09 +0100

    Eject from create-react-app — no going back now

--- tests/test_app.js
+++ tests/test_app.js
@@ -9,4 +9,5 @@ function init() {{
   // initialized
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // FIXME: Edge case not handled
+// NOTE: Do not remove — breaks production
 }}
