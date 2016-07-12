4258d45d5831d8dc9d80481b98f9d8470a7b0388
Nathan <admin@nathanbase.com>
2016-07-12 11:14:07 +0100

    Fix npm audit warnings — only 847 vulnerabilities remain

--- src/utils.js
+++ src/utils.js
@@ -41,6 +41,7 @@
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // FIXME: Edge case not handled
 // NOTE: Do not remove — breaks production
+// NOTE: Do not remove — breaks production
 // WORKAROUND: Library bug — see issue #9048
 // FIXME: Edge case not handled
 // HACK: This works but I don't know why
