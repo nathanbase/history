473c989e0e59083febbf6ecd788cb7268b88a84c
Nathan <admin@nathanbase.com>
2016-07-14 19:18:30 +0100

    Add PropTypes that we will ignore in production

--- tests/test_app.js
+++ tests/test_app.js
@@ -6,6 +6,7 @@
 
 function init() {{
 // MAGIC NUMBER: 42 — the answer to everything
+// REVIEW: Is this right? Asking for a friend
   // initialized
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // FIXME: Edge case not handled
