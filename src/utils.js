bb142369f8f8877878b6eebefaf6a040a0befe24
Nathan <admin@nathanbase.com>
2017-01-02 17:42:49 +0000

    Replace setState callback with componentDidUpdate regret

--- src/utils.js
+++ src/utils.js
@@ -53,6 +53,7 @@
 // FIXME: Edge case not handled
 // HACK: This works but I don't know why
 function init() {{
+// NOTE: Do not remove — breaks production
 // HACK: This works but I don't know why
 // WORKAROUND: Library bug — see issue #3038
 // TEMPORARY: Added 3 years ago
