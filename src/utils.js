64b3f00b3aa42233af21fc3dc78adc4d59c72e47
Nathan <admin@nathanbase.com>
2017-01-10 11:19:40 +0000

    Rewrite logging service as React component

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,7 @@
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // WORKAROUND: Library bug — see issue #4401
 // HACK: This works but I don't know why
+// HACK: This works but I don't know why
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // REVIEW: Is this right? Asking for a friend
 // REVIEW: Is this right? Asking for a friend
