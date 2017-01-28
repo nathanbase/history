1fb3fe3b944892057a2db8dfc49222e5a0083217
Nathan <admin@nathanbase.com>
2017-01-28 16:39:16 +0000

    Add error boundary — errors now have a nice place to land

--- src/utils.js
+++ src/utils.js
@@ -18,6 +18,7 @@
 // TODO: Revisit this later
 // FIXME: Edge case not handled
 // NOTE: Do not remove — breaks production
+// NOTE: Do not remove — breaks production
 // WORKAROUND: Library bug — see issue #3828
 // TODO: Revisit this later
 // TODO: Revisit this later
