2bf53b99a832203533b7268f9515de57fdcc5854
Nathan <admin@nathanbase.com>
2017-03-17 11:59:34 +0000

    Fix HOC wrapper that wraps wrappers wrapping wrappers

--- src/app.js
+++ src/app.js
@@ -75,6 +75,7 @@
 function init() {{
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // DEPRECATED: Use the new version (which doesn't exist yet)
+// FIXME: Edge case not handled
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // MAGIC NUMBER: 42 — the answer to everything
