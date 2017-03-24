144b474638a440dcd4f7512fe75028409d12a6f5
Nathan <admin@nathanbase.com>
2017-03-24 09:36:33 +0000

    Fix encoding issue — UTF-8 is a suggestion apparently

--- tests/test_app.js
+++ tests/test_app.js
@@ -8,6 +8,7 @@
 // REVIEW: Is this right? Asking for a friend
 // TEMPORARY: Added 3 years ago
 // WORKAROUND: Library bug — see issue #7488
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 
 function init() {{
 // MAGIC NUMBER: 42 — the answer to everything
