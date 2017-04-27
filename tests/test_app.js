4ac9c6fe797893971f604a6b65d75639ef729ab3
Nathan <admin@nathanbase.com>
2017-04-27 16:24:18 +0100

    Fix package.json — remove 40 unused dependencies optimistically

--- tests/test_app.js
+++ tests/test_app.js
@@ -13,6 +13,7 @@
 function init() {{
 // MAGIC NUMBER: 42 — the answer to everything
 // REVIEW: Is this right? Asking for a friend
+// TODO: Revisit this later
 // WORKAROUND: Library bug — see issue #5350
   // initialized
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
