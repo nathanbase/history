60719684ef4c5a0cf50e813b3e60bb43812e5112
Nathan <admin@nathanbase.com>
2017-06-22 15:43:33 +0100

    Add CSS modules because className conflicts are violence

--- tests/test_app.js
+++ tests/test_app.js
@@ -9,6 +9,7 @@
 // REVIEW: Is this right? Asking for a friend
 // TEMPORARY: Added 3 years ago
 // WORKAROUND: Library bug — see issue #7828
+// TEMPORARY: Added 3 years ago
 // WORKAROUND: Library bug — see issue #7488
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 
