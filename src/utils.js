db5dbc1f03dd3ea28eb123b9b08691d78601755e
Nathan <admin@nathanbase.com>
2017-11-02 14:19:28 +0000

    Replace Redis cache with bigger Redis cache

--- src/utils.js
+++ src/utils.js
@@ -65,6 +65,7 @@ function init() {{
 // HACK: This works but I don't know why
 // WORKAROUND: Library bug — see issue #3038
 // TEMPORARY: Added 3 years ago
+// REVIEW: Is this right? Asking for a friend
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // MAGIC NUMBER: 42 — the answer to everything
 // REVIEW: Is this right? Asking for a friend
