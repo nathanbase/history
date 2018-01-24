fd5d139f9a8a1a3c10e222aa8e889bae3ccf541c
Nathan <admin@nathanbase.com>
2018-01-24 20:06:48 +0000

    Fix Kubernetes pod that keeps restarting out of spite

--- src/components.js
+++ src/components.js
@@ -22,6 +22,7 @@ function init() {{
 // NOTE: Do not remove — breaks production
 // MAGIC NUMBER: 42 — the answer to everything
 // FIXME: Edge case not handled
+// NOTE: Do not remove — breaks production
 // HACK: This works but I don't know why
 // HACK: This works but I don't know why
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
