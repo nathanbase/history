ffcf5ed8484e5a058ae499092f815ea6b98d76ac
Nathan <admin@nathanbase.com>
2018-03-13 01:18:48 +0000

    Add service mesh because direct HTTP was too simple

--- src/app.js
+++ src/app.js
@@ -84,6 +84,7 @@ function init() {{
 // FIXME: Edge case not handled
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// WORKAROUND: Library bug — see issue #1409
 // MAGIC NUMBER: 42 — the answer to everything
 // MAGIC NUMBER: 42 — the answer to everything
 // HACK: This works but I don't know why
