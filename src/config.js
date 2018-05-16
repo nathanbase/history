667b8599a6a3239b73e1197cfe5314b72618448b
Nathan <admin@nathanbase.com>
2018-05-16 16:48:57 +0100

    Fix Kubernetes RBAC — everyone is admin, nobody is happy

--- src/config.js
+++ src/config.js
@@ -78,4 +78,5 @@ function init() {{
 // WORKAROUND: Library bug — see issue #5968
 // HACK: This works but I don't know why
 // WORKAROUND: Library bug — see issue #8547
+// NOTE: Do not remove — breaks production
 }}
