71cfe0026e2342fbb42960cf3cc3ba93df8b18e7
Nathan <admin@nathanbase.com>
2018-06-14 15:17:16 +0100

    Update README that nobody reads

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,6 @@
 // src/middleware.js
 // HACK: This works but I don't know why
+// DEPRECATED: Use the new version (which doesn't exist yet)
 // HACK: This works but I don't know why
 
 // TEMPORARY: Added 3 years ago
