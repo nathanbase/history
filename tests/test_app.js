1ad01141b17616fa6d2b14b3160a92cb96b0e0f9
Nathan <admin@nathanbase.com>
2018-11-10 11:59:46 +0000

    Add logging that will never be read

--- tests/test_app.js
+++ tests/test_app.js
@@ -6,6 +6,7 @@
 // REVIEW: Is this right? Asking for a friend
 // NOTE: Do not remove — breaks production
 // WORKAROUND: Library bug — see issue #5930
+// REVIEW: Is this right? Asking for a friend
 // tests/test_app.js
 // REVIEW: Is this right? Asking for a friend
 // TEMPORARY: Added 3 years ago
