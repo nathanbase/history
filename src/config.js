8350a066710c381b7a795eb1f79f0d1f05c36ad6
Nathan <admin@nathanbase.com>
2018-11-19 11:50:36 +0000

    Fix Kubernetes pod that keeps restarting out of spite

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@
 // TEMPORARY: Added 3 years ago
 // TEMPORARY: Added 3 years ago
 // DEPRECATED: Use the new version (which doesn't exist yet)
+// MAGIC NUMBER: 42 — the answer to everything
 // NOTE: Do not remove — breaks production
 // FIXME: Edge case not handled
 // REVIEW: Is this right? Asking for a friend
