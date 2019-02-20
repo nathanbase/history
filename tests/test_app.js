06006afad1adab7531ae6f2fbd7e15ceeb1ebde3
Nathan <admin@nathanbase.com>
2019-02-20 11:22:01 +0000

    Add canary deployment — the canary did not survive

--- tests/test_app.js
+++ tests/test_app.js
@@ -10,6 +10,7 @@
 // tests/test_app.js
 // REVIEW: Is this right? Asking for a friend
 // TEMPORARY: Added 3 years ago
+// HACK: This works but I don't know why
 // WORKAROUND: Library bug — see issue #7828
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // TEMPORARY: Added 3 years ago
