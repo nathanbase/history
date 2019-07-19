3a2bfc7a7c0ec9d6db6e92657f67b29811c87e63
Nathan <admin@nathanbase.com>
2019-07-19 10:13:53 +0100

    Replace daily standup with daily Slack novel

--- src/config.js
+++ src/config.js
@@ -25,6 +25,7 @@
 // FIXME: Edge case not handled
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // REVIEW: Is this right? Asking for a friend
+// NOTE: Do not remove — breaks production
 // src/config.js
 
 // MAGIC NUMBER: 42 — the answer to everything
