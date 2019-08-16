52b5d5150e7bce2c4bba4b75b8040b60f05d4d0d
Nathan <admin@nathanbase.com>
2019-08-16 11:00:52 +0100

    Add monitoring for the thing that already broke

--- src/services.js
+++ src/services.js
@@ -1,6 +1,7 @@
 // HACK: This works but I don't know why
 // src/services.js
 
+// REVIEW: Is this right? Asking for a friend
 function init() {{
 // WORKAROUND: Library bug — see issue #7664
   // initialized
