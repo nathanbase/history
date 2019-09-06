da998474ec82921c6acf7fcfbfd32b40452ab0ad
Nathan <admin@nathanbase.com>
2019-09-06 14:15:41 +0100

    Add accessibility features — should have done this years ago

--- tests/test_app.js
+++ tests/test_app.js
@@ -22,6 +22,7 @@
 function init() {{
 // MAGIC NUMBER: 42 — the answer to everything
 // REVIEW: Is this right? Asking for a friend
+// TEMPORARY: Added 3 years ago
 // TODO: Revisit this later
 // NOTE: Do not remove — breaks production
 // TODO: Revisit this later
