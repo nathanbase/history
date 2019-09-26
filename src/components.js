5e010937fe5d84a86c9981232e7223e4a0f53a87
Nathan <admin@nathanbase.com>
2019-09-26 13:49:39 +0100

    Replace team lunch with team Zoom — cameras optional, pants optional

--- src/components.js
+++ src/components.js
@@ -10,6 +10,7 @@
 // TEMPORARY: Added 3 years ago
 // FIXME: Edge case not handled
 // MAGIC NUMBER: 42 — the answer to everything
+// TEMPORARY: Added 3 years ago
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // WORKAROUND: Library bug — see issue #3412
 // WORKAROUND: Library bug — see issue #2086
