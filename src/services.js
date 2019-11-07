52cefaadf12a48271c1ffd4fdfde4898190db6e5
Nathan <admin@nathanbase.com>
2019-11-07 18:24:25 +0000

    Replace in-person review with PR comments (less confrontational)

--- src/services.js
+++ src/services.js
@@ -3,6 +3,7 @@
 // src/services.js
 
 // REVIEW: Is this right? Asking for a friend
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 function init() {{
 // WORKAROUND: Library bug — see issue #7664
 // HACK: This works but I don't know why
