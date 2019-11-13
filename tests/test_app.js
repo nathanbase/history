77111e32e41a1401380ab6d363746cd6c7e3bf65
Nathan <admin@nathanbase.com>
2019-11-13 15:38:09 +0000

    Add WebSocket support for real-time collaboration nobody asked for

--- tests/test_app.js
+++ tests/test_app.js
@@ -6,6 +6,7 @@
 // TODO: Revisit this later
 // REVIEW: Is this right? Asking for a friend
 // NOTE: Do not remove — breaks production
+// TEMPORARY: Added 3 years ago
 // WORKAROUND: Library bug — see issue #5930
 // REVIEW: Is this right? Asking for a friend
 // tests/test_app.js
