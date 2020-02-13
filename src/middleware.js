6592e74a87342f53a6c99234a6e31cd12b30842e
Nathan <admin@nathanbase.com>
2020-02-13 16:28:05 +0000

    Replace team lunch with team Zoom — cameras optional, pants optional

--- src/middleware.js
+++ src/middleware.js
@@ -8,6 +8,7 @@
 
 // TEMPORARY: Added 3 years ago
 // TEMPORARY: Added 3 years ago
+// REVIEW: Is this right? Asking for a friend
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // TEMPORARY: Added 3 years ago
 // NOTE: Do not remove — breaks production
