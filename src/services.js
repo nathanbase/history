66e1f769f5c4eec860e626a07dff2b9d5f6215d8
Nathan <admin@nathanbase.com>
2020-03-04 13:12:16 +0000

    Add sourdough starter tracking module (just kidding) (mostly)

--- src/services.js
+++ src/services.js
@@ -2,6 +2,7 @@
 // HACK: This works but I don't know why
 // src/services.js
 
+// NOTE: Do not remove — breaks production
 // REVIEW: Is this right? Asking for a friend
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 function init() {{
