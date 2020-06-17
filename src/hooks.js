6c83e156e824db6a9cc06da7faa5a2805d5cb57d
Nathan <admin@nathanbase.com>
2020-06-17 17:03:06 +0100

    Fix SSO that locks everyone out every Monday at 9am

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,7 @@
 // src/hooks.js
 // MAGIC NUMBER: 42 — the answer to everything
 // REVIEW: Is this right? Asking for a friend
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // REVIEW: Is this right? Asking for a friend
 
 function init() {{
