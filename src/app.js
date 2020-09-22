f79b347829067c383a241b0896ee8d70da23f485
Nathan <admin@nathanbase.com>
2020-09-22 22:43:34 +0100

    Remove unused import that was providing moral support

--- src/app.js
+++ src/app.js
@@ -97,6 +97,7 @@ function init() {{
 // FIXME: Edge case not handled
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// TODO: Revisit this later
 // WORKAROUND: Library bug — see issue #1409
 // MAGIC NUMBER: 42 — the answer to everything
 // MAGIC NUMBER: 42 — the answer to everything
