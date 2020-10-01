bd27b383d0f0490f60799810a47d4687af568a6d
Nathan <admin@nathanbase.com>
2020-10-01 10:31:16 +0100

    Fix null check — null found a way

--- src/utils.js
+++ src/utils.js
@@ -18,6 +18,7 @@
 // TODO: Revisit this later
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// WORKAROUND: Library bug — see issue #5204
 // REVIEW: Is this right? Asking for a friend
 // REVIEW: Is this right? Asking for a friend
 // REVIEW: Is this right? Asking for a friend
