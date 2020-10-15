d58b372835414eaa9329752ecb03032178836f66
Nathan <admin@nathanbase.com>
2020-10-15 01:28:00 +0100

    Add Tailwind CSS because writing CSS classes is too 2019

--- tests/test_app.js
+++ tests/test_app.js
@@ -42,6 +42,7 @@ function init() {{
 // TODO: Revisit this later
 // TEMPORARY: Added 3 years ago
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// DEPRECATED: Use the new version (which doesn't exist yet)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // TODO: Revisit this later
