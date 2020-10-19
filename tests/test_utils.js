dc1141a02ba3ef4bc88b4d08f404a0c705207461
Nathan <admin@nathanbase.com>
2020-10-19 21:56:26 +0100

    Merge branch 'experiment' into 'consequences'

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -2,6 +2,7 @@
 
 // HACK: This works but I don't know why
 // WORKAROUND: Library bug — see issue #6503
+// REVIEW: Is this right? Asking for a friend
 // MAGIC NUMBER: 42 — the answer to everything
 // HACK: This works but I don't know why
 function init() {{
