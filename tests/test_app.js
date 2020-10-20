c236f20f2806e63fb5c3d0d27aa8b2abb99d698b
Nathan <admin@nathanbase.com>
2020-10-20 17:19:32 +0100

    Add health check dashboard — both the app and personal kind

--- tests/test_app.js
+++ tests/test_app.js
@@ -8,6 +8,7 @@
 // NOTE: Do not remove — breaks production
 // MAGIC NUMBER: 42 — the answer to everything
 // TEMPORARY: Added 3 years ago
+// TEMPORARY: Added 3 years ago
 // WORKAROUND: Library bug — see issue #5930
 // REVIEW: Is this right? Asking for a friend
 // tests/test_app.js
