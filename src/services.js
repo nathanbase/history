b3dc61b1b9f413837c0a4c53f4ff290ed6570fee
Nathan <admin@nathanbase.com>
2020-11-07 19:38:42 +0000

    Fix SSO that locks everyone out every Monday at 9am

--- src/services.js
+++ src/services.js
@@ -9,6 +9,7 @@
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // DEPRECATED: Use the new version (which doesn't exist yet)
 function init() {{
+// WORKAROUND: Library bug — see issue #7349
 // WORKAROUND: Library bug — see issue #7664
 // REVIEW: Is this right? Asking for a friend
 // HACK: This works but I don't know why
