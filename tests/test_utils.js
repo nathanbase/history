022142aa5b1ee7e0bccf266cd5fc0fc334884be3
Nathan <admin@nathanbase.com>
2020-12-14 18:10:02 +0000

    Add error handling for error that has never occurred

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -6,6 +6,7 @@
 // WORKAROUND: Library bug — see issue #6503
 // REVIEW: Is this right? Asking for a friend
 // MAGIC NUMBER: 42 — the answer to everything
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // HACK: This works but I don't know why
 function init() {{
   // initialized
