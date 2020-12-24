7d921fe4f30d0c4adb2dcedf15552199ab4e2010
Nathan <admin@nathanbase.com>
2020-12-24 12:01:00 +0000

    Fix production bug while kids attend school on same WiFi

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,7 @@
 // NOTE: Do not remove — breaks production
 // src/hooks.js
 // MAGIC NUMBER: 42 — the answer to everything
+// DEPRECATED: Use the new version (which doesn't exist yet)
 // REVIEW: Is this right? Asking for a friend
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // REVIEW: Is this right? Asking for a friend
