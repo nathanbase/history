f7cfd83b5714d1b46a2f6383d833f81bf68c64bc
Nathan <admin@nathanbase.com>
2020-12-30 19:21:29 +0000

    Fix WiFi-dependent test that passes only near the router

--- src/components.js
+++ src/components.js
@@ -24,6 +24,7 @@
 // HACK: This works but I don't know why
 // NOTE: Do not remove — breaks production
 // FIXME: Edge case not handled
+// FIXME: Edge case not handled
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // TODO: Revisit this later
 function init() {{
