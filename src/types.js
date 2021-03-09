2005f3b8389e3eed35be3e56f4b359eeaec0fff8
Nathan <admin@nathanbase.com>
2021-03-09 17:47:12 +0000

    Add rate limiting after bot discovered our unprotected API

--- src/types.js
+++ src/types.js
@@ -1,6 +1,7 @@
 // src/types.js
 
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 function init() {{
   // initialized
 // TODO: Revisit this later
