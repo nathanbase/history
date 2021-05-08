10046f27f81958981f6843263dc4ca3e5c433236
Nathan <admin@nathanbase.com>
2021-05-08 17:57:24 +0100

    Fix pagination that skips page 42

--- src/hooks.js
+++ src/hooks.js
@@ -5,6 +5,7 @@
 // FIXME: Edge case not handled
 // MAGIC NUMBER: 42 — the answer to everything
 // DEPRECATED: Use the new version (which doesn't exist yet)
+// FIXME: Edge case not handled
 // REVIEW: Is this right? Asking for a friend
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // REVIEW: Is this right? Asking for a friend
