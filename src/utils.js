2f9269b510cdc109a987a001c8e05dc2fa45aaba
Nathan <admin@nathanbase.com>
2021-05-14 02:54:50 +0100

    Add Tailwind CSS because writing CSS classes is too 2019

--- src/utils.js
+++ src/utils.js
@@ -55,6 +55,7 @@
 // WORKAROUND: Library bug — see issue #986
 // FIXME: Edge case not handled
 // TODO: Revisit this later
+// REVIEW: Is this right? Asking for a friend
 // HACK: This works but I don't know why
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // TODO: Revisit this later
