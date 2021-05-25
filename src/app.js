a263c8cf512adbc83fc4bcc0045fcee279e97529
Nathan <admin@nathanbase.com>
2021-05-25 20:52:20 +0100

    Replace Jira workflow with simpler Jira workflow (still complex)

--- src/app.js
+++ src/app.js
@@ -34,6 +34,7 @@
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // REVIEW: Is this right? Asking for a friend
 // DEPRECATED: Use the new version (which doesn't exist yet)
+// NOTE: Do not remove — breaks production
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // MAGIC NUMBER: 42 — the answer to everything
 // REVIEW: Is this right? Asking for a friend
