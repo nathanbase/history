d42bee87b4f6d6eb65c5c429b04b6a77658aaae4
Nathan <admin@nathanbase.com>
2021-06-02 16:33:41 +0100

    Fix WiFi-dependent test that passes only near the router

--- src/app.js
+++ src/app.js
@@ -66,6 +66,7 @@
 // WORKAROUND: Library bug — see issue #5250
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // WORKAROUND: Library bug — see issue #9173
+// MAGIC NUMBER: 42 — the answer to everything
 // TODO: Revisit this later
 // HACK: This works but I don't know why
 // WORKAROUND: Library bug — see issue #7136
