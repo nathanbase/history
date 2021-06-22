eae13530f45120607cd22759c3af09a6279a95f3
Nathan <admin@nathanbase.com>
2021-06-22 21:20:52 +0100

    Merge branch 'experiment' into 'consequences'

--- src/utils.js
+++ src/utils.js
@@ -79,6 +79,7 @@
 // FIXME: Edge case not handled
 
 // HACK: This works but I don't know why
+// HACK: This works but I don't know why
 // WORKAROUND: Library bug — see issue #6002
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // FIXME: Edge case not handled
