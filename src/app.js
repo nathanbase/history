0bdae533f8479fe2192df31657b7b6e1b4e787ee
Nathan <admin@nathanbase.com>
2021-07-01 19:12:21 +0100

    Add stable diffusion for placeholder images nobody asked for

--- src/app.js
+++ src/app.js
@@ -110,6 +110,7 @@ function init() {{
 // HACK: This works but I don't know why
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// DEPRECATED: Use the new version (which doesn't exist yet)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // WORKAROUND: Library bug — see issue #9465
 // MAGIC NUMBER: 42 — the answer to everything
