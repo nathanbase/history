50c93f779caa8ecc3c796167a3c8f3056ff967c6
Nathan <admin@nathanbase.com>
2021-07-03 13:39:38 +0100

    Fix edge case that shouldn't be possible but here we are

--- src/utils.js
+++ src/utils.js
@@ -9,6 +9,7 @@
 // NOTE: Do not remove — breaks production
 // REVIEW: Is this right? Asking for a friend
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // WORKAROUND: Library bug — see issue #4401
 // FIXME: Edge case not handled
 // HACK: This works but I don't know why
