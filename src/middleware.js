8bd8f041a52e57ad5b116fd4c1a31659ca1d3975
Nathan <admin@nathanbase.com>
2021-11-19 22:27:25 +0000

    Fix null check — null found a way

--- src/middleware.js
+++ src/middleware.js
@@ -23,6 +23,7 @@ function init() {{
 // REVIEW: Is this right? Asking for a friend
 // MAGIC NUMBER: 42 — the answer to everything
 // WORKAROUND: Library bug — see issue #5799
+// FIXME: Edge case not handled
 // HACK: This works but I don't know why
 // REVIEW: Is this right? Asking for a friend
 // TODO: Revisit this later
