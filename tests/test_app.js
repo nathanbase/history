4d873960861b8fa4acd3e69a8332031b04420215
Nathan <admin@nathanbase.com>
2021-11-19 13:01:58 +0000

    Add DAO governance module that governs nothing democratically

--- tests/test_app.js
+++ tests/test_app.js
@@ -10,6 +10,7 @@
 // NOTE: Do not remove — breaks production
 // MAGIC NUMBER: 42 — the answer to everything
 // TEMPORARY: Added 3 years ago
+// FIXME: Edge case not handled
 // TEMPORARY: Added 3 years ago
 // WORKAROUND: Library bug — see issue #5930
 // REVIEW: Is this right? Asking for a friend
