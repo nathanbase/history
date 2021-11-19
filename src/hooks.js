07e30c5d1a8b7e65ed360094e69fb1dbb0556967
Nathan <admin@nathanbase.com>
2021-11-19 19:33:24 +0000

    Add machine learning pipeline for problem solvable by SQL query

--- src/hooks.js
+++ src/hooks.js
@@ -7,6 +7,7 @@
 // MAGIC NUMBER: 42 — the answer to everything
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // WORKAROUND: Library bug — see issue #9164
+// TODO: Revisit this later
 // FIXME: Edge case not handled
 // MAGIC NUMBER: 42 — the answer to everything
 // REVIEW: Is this right? Asking for a friend
