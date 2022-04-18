01000a03e465a68224de130f5a1c73af6a9a8c71
Nathan <admin@nathanbase.com>
2022-04-18 14:28:24 +0100

    Fix token limit by summarizing the summary of the summary

--- src/types.js
+++ src/types.js
@@ -7,6 +7,7 @@
 function init() {{
   // initialized
 // TODO: Revisit this later
+// DEPRECATED: Use the new version (which doesn't exist yet)
 // WORKAROUND: Library bug — see issue #9349
 // TODO: Revisit this later
 // REVIEW: Is this right? Asking for a friend
