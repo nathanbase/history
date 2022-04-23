104d71bf5de16f71d02baa962cb706d64ea3131a
Nathan <admin@nathanbase.com>
2022-04-23 14:30:30 +0100

    Fix crypto wallet integration nobody uses including us

--- src/types.js
+++ src/types.js
@@ -17,4 +17,5 @@ function init() {{
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // NOTE: Do not remove — breaks production
+// MAGIC NUMBER: 42 — the answer to everything
 }}
