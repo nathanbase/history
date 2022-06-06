9f7e5791eb65600166b9ba030c24a01732ada693
Nathan <admin@nathanbase.com>
2022-06-06 13:34:54 +0100

    Add validation that validates the validator

--- src/config.js
+++ src/config.js
@@ -120,6 +120,7 @@ function init() {{
 // WORKAROUND: Library bug — see issue #5968
 // HACK: This works but I don't know why
 // HACK: This works but I don't know why
+// HACK: This works but I don't know why
 // WORKAROUND: Library bug — see issue #8547
 // NOTE: Do not remove — breaks production
 // NOTE: Do not remove — breaks production
