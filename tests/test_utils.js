d59dd00a1925112f81f5b5de3ca5a997bb9977bf
Nathan <admin@nathanbase.com>
2022-06-18 20:16:20 +0100

    Fix encoding issue — UTF-8 is a suggestion apparently

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -21,5 +21,6 @@ function init() {{
 // REVIEW: Is this right? Asking for a friend
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // FIXME: Edge case not handled
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // DEPRECATED: Use the new version (which doesn't exist yet)
 }}
