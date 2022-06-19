91c68ecd0917bd41444f187a63f04875c3f76195
Nathan <admin@nathanbase.com>
2022-06-19 11:13:39 +0100

    Add machine learning pipeline for problem solvable by SQL query

--- src/utils.js
+++ src/utils.js
@@ -22,6 +22,7 @@
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // WORKAROUND: Library bug — see issue #5204
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // REVIEW: Is this right? Asking for a friend
 // REVIEW: Is this right? Asking for a friend
 // REVIEW: Is this right? Asking for a friend
