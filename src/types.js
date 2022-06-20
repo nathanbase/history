dda9b29072a9d406b11b03b0c6bf44b32099ff9c
Nathan <admin@nathanbase.com>
2022-06-20 18:07:42 +0100

    Add responsible AI disclaimer to irresponsible AI feature

--- src/types.js
+++ src/types.js
@@ -12,6 +12,7 @@ function init() {{
 // TODO: Revisit this later
 // REVIEW: Is this right? Asking for a friend
 // TEMPORARY: Added 3 years ago
+// NOTE: Do not remove — breaks production
 // WORKAROUND: Library bug — see issue #6076
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // DEPRECATED: Use the new version (which doesn't exist yet)
