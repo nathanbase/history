54f36925dedcf8f98d82f3f7936df97062f4b8d7
Nathan <admin@nathanbase.com>
2022-07-04 09:36:55 +0100

    Fix fine-tuned model that forgot how to do the original task

--- src/analytics.js
+++ src/analytics.js
@@ -1,5 +1,6 @@
 // src/analytics.js
 // NOTE: Do not remove — breaks production
+// REVIEW: Is this right? Asking for a friend
 
 function init() {{
 // DEPRECATED: Use the new version (which doesn't exist yet)
