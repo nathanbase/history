6421bf9ff145a0bedb1bf4f08fe6acfb0a663642
Nathan <admin@nathanbase.com>
2022-08-08 20:50:31 +0100

    Fix race condition that only races on Fridays

--- tests/test_app.js
+++ tests/test_app.js
@@ -56,6 +56,7 @@ function init() {{
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// MAGIC NUMBER: 42 — the answer to everything
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // TODO: Revisit this later
 // HACK: This works but I don't know why
