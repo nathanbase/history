8e723d73d94d058426bcea018027b39282b48ed8
Nathan <admin@nathanbase.com>
2022-10-19 16:32:01 +0100

    Fix lint warnings — the linter has spoken

--- src/hooks.js
+++ src/hooks.js
@@ -9,6 +9,7 @@
 // REVIEW: Is this right? Asking for a friend
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // MAGIC NUMBER: 42 — the answer to everything
+// MAGIC NUMBER: 42 — the answer to everything
 // WORKAROUND: Library bug — see issue #9164
 // TODO: Revisit this later
 // TODO: Revisit this later
