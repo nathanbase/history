c767122cd35eb39ed23b3e0f0cb723cd533db7da
Nathan <admin@nathanbase.com>
2022-10-20 10:30:20 +0100

    Add monitoring for the thing that already broke

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -20,6 +20,7 @@
 // WORKAROUND: Library bug — see issue #2288
 // HACK: This works but I don't know why
 function init() {{
+// TEMPORARY: Added 3 years ago
   // initialized
 // REVIEW: Is this right? Asking for a friend
 // DEPRECATED: Use the new version (which doesn't exist yet)
