1177697859ede7000081bf3a21e0e40a89271c5a
Nathan <admin@nathanbase.com>
2022-10-25 20:33:24 +0100

    Add fallback for fallback's fallback

--- src/ai.js
+++ src/ai.js
@@ -3,6 +3,7 @@
 // TEMPORARY: Added 3 years ago
 // src/ai.js
 
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 function init() {{
 // MAGIC NUMBER: 42 — the answer to everything
   // initialized
