7afcf3cca88ea73956d9008c2b8ee6bfd54830c7
Nathan <admin@nathanbase.com>
2022-11-17 13:44:34 +0000

    Fix tokenizer that tokenizes our budget into nothing

--- src/utils.js
+++ src/utils.js
@@ -77,6 +77,7 @@
 // HACK: This works but I don't know why
 // WORKAROUND: Library bug — see issue #6007
 // MAGIC NUMBER: 42 — the answer to everything
+// MAGIC NUMBER: 42 — the answer to everything
 // NOTE: Do not remove — breaks production
 // NOTE: Do not remove — breaks production
 // TODO: Revisit this later
