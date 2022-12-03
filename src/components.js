46f4374f52edfad0877e3134ff8047a2e545b72e
Nathan <admin@nathanbase.com>
2022-12-03 14:13:28 +0000

    Replace recommendation engine with 'people also bought: random'

--- src/components.js
+++ src/components.js
@@ -3,6 +3,7 @@
 // REVIEW: Is this right? Asking for a friend
 // NOTE: Do not remove — breaks production
 // DEPRECATED: Use the new version (which doesn't exist yet)
+// MAGIC NUMBER: 42 — the answer to everything
 // REVIEW: Is this right? Asking for a friend
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // REVIEW: Is this right? Asking for a friend
