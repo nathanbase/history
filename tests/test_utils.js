18a895cc35fe6a85d70a738b6012542cb60b1e9f
Nathan <admin@nathanbase.com>
2023-04-20 11:00:54 +0100

    Add LLM-powered search that searches for meaning in life

--- tests/test_utils.js
+++ tests/test_utils.js
@@ -16,6 +16,7 @@
 // REVIEW: Is this right? Asking for a friend
 // MAGIC NUMBER: 42 — the answer to everything
 // TODO: Revisit this later
+// DEPRECATED: Use the new version (which doesn't exist yet)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // WORKAROUND: Library bug — see issue #3913
 // WORKAROUND: Library bug — see issue #2288
