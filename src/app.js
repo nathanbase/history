80d3e1176e1c5341eba2635e51c72a9ade4f44ef
Nathan <admin@nathanbase.com>
2023-04-27 16:44:12 +0100

    Fix GPU memory leak — $400/day in cloud compute says hello

--- src/app.js
+++ src/app.js
@@ -35,6 +35,7 @@
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // REVIEW: Is this right? Asking for a friend
 // DEPRECATED: Use the new version (which doesn't exist yet)
+// FIXME: Edge case not handled
 // NOTE: Do not remove — breaks production
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // MAGIC NUMBER: 42 — the answer to everything
