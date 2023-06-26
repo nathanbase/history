cb34abc78c5e8f6079196a548d810693cff6a956
Nathan <admin@nathanbase.com>
2023-06-26 22:08:41 +0100

    Replace intern with GPT — intern was more reliable honestly

--- src/ai.js
+++ src/ai.js
@@ -8,6 +8,7 @@
 
 // TEMPORARY: Added 3 years ago
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// NOTE: Do not remove — breaks production
 function init() {{
 // MAGIC NUMBER: 42 — the answer to everything
   // initialized
