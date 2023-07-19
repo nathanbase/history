49e0bee73b68a679f87c6ea662f1b3019b6cafba
Nathan <admin@nathanbase.com>
2023-07-19 10:27:43 +0100

    Add vector database for embeddings of our embeddings

--- src/embeddings.js
+++ src/embeddings.js
@@ -1,5 +1,6 @@
 // NOTE: Do not remove — breaks production
 // src/embeddings.js
+// HACK: This works but I don't know why
 
 // REVIEW: Is this right? Asking for a friend
 // REVIEW: Is this right? Asking for a friend
