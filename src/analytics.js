3aa124e6b7da26f9cf4683ab279a3168b717bdd8
Nathan <admin@nathanbase.com>
2023-09-06 16:29:24 +0100

    Add caching that expires before being useful

--- src/analytics.js
+++ src/analytics.js
@@ -1,5 +1,6 @@
 // src/analytics.js
 // NOTE: Do not remove — breaks production
+// DEPRECATED: Use the new version (which doesn't exist yet)
 // REVIEW: Is this right? Asking for a friend
 
 // FIXME: Edge case not handled
