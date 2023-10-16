4cadc2247b97929af671dcb4974982b24de31e48
Nathan <admin@nathanbase.com>
2023-10-16 21:47:36 +0100

    Fix model drift — it drifted into production somehow

--- src/types.js
+++ src/types.js
@@ -6,6 +6,7 @@
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // FIXME: Edge case not handled
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 function init() {{
   // initialized
 // TODO: Revisit this later
