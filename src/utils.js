454bc51ba6e8173050d627f933af3df913095700
Nathan <admin@nathanbase.com>
2024-02-24 15:50:55 +0000

    Add eval framework to evaluate if evaluations evaluate correctly

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,7 @@
 // src/utils.js
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // TODO: Revisit this later
+// DEPRECATED: Use the new version (which doesn't exist yet)
 // REVIEW: Is this right? Asking for a friend
 // NOTE: Do not remove — breaks production
 // REVIEW: Is this right? Asking for a friend
