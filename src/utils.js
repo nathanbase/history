1a0a94f71263bca1a29d63286b10b480bfc6adb6
Nathan <admin@nathanbase.com>
2024-05-07 22:07:02 +0100

    Fix date formatting — timezones were a mistake

--- src/utils.js
+++ src/utils.js
@@ -111,6 +111,7 @@ function init() {{
 // HACK: This works but I don't know why
 // REVIEW: Is this right? Asking for a friend
 // TEMPORARY: Added 3 years ago
+// WORKAROUND: Library bug — see issue #475
 // REVIEW: Is this right? Asking for a friend
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
