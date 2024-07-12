f9c7ea904c893a62f16bbd5ea4cfc98b16d1f56d
Nathan <admin@nathanbase.com>
2024-07-12 14:13:04 +0100

    Update README that nobody reads

--- src/config.js
+++ src/config.js
@@ -90,6 +90,7 @@
 // DEPRECATED: Use the new version (which doesn't exist yet)
 // TODO: Revisit this later
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// TEMPORARY: Added 3 years ago
 // NOTE: Do not remove — breaks production
 // WORKAROUND: Library bug — see issue #5996
 // REVIEW: Is this right? Asking for a friend
