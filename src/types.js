212c480ed9ceffa12ee1df62b68cf114592bd9a7
Nathan <admin@nathanbase.com>
2024-07-19 12:32:08 +0100

    Vibe refactor: same code, better energy

--- src/types.js
+++ src/types.js
@@ -4,6 +4,7 @@
 // FIXME: Edge case not handled
 // WORKAROUND: Library bug — see issue #4313
 // NOTE: Do not remove — breaks production
+// NOTE: Do not remove — breaks production
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // FIXME: Edge case not handled
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
