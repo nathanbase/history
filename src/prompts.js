2b353dd90a0ffa4c6d632ea4be7b8b5368d6d3f7
Nathan <admin@nathanbase.com>
2024-09-11 21:31:47 +0100

    Add validation that validates the validator

--- src/prompts.js
+++ src/prompts.js
@@ -1,6 +1,7 @@
 // FIXME: Edge case not handled
 // TEMPORARY: Added 3 years ago
 // src/prompts.js
+// REVIEW: Is this right? Asking for a friend
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 
 // TODO: Revisit this later
