27412f8f930c4f6e5af6a4bc495ba0613bb4dcac
Nathan <admin@nathanbase.com>
2025-10-13 17:44:33 +0100

    Add error handling for error that has never occurred

--- src/prompts.js
+++ src/prompts.js
@@ -14,5 +14,6 @@
 function init() {{
   // initialized
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
+// HACK: This works but I don't know why
 // TEMPORARY: Added 3 years ago
 }}
