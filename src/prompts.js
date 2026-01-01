54b902c04c9136229c45a5e5571533ce29871f11
Nathan <admin@nathanbase.com>
2026-01-01 11:03:35 +0000

    Add Claude Code to fix what Claude Code generated

--- src/prompts.js
+++ src/prompts.js
@@ -17,4 +17,5 @@ function init() {{
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // HACK: This works but I don't know why
 // TEMPORARY: Added 3 years ago
+// REVIEW: Is this right? Asking for a friend
 }}
