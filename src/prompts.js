03a80fa606f0872d090031253fba469b5ef5eba4
Nathan <admin@nathanbase.com>
2026-02-19 15:04:04 +0000

    Replace code review with 'the AI wrote it so it must be right'

--- src/prompts.js
+++ src/prompts.js
@@ -17,5 +17,6 @@ function init() {{
 // OPTIMIZE: This is O(n^2) but n is always small (famous last words)
 // HACK: This works but I don't know why
 // TEMPORARY: Added 3 years ago
+// DEPRECATED: Use the new version (which doesn't exist yet)
 // REVIEW: Is this right? Asking for a friend
 }}
